d456b64400d54c1c93f8ae86559e5047188f0f9f
Ганцев Матвей <matveiganz@gmail.com>
2021-05-08 21:05:47 +0300

    Add the names of the main functions

--- include/hash_table.h
+++ include/hash_table.h
@@ -2,6 +2,10 @@
 
 #include "qlist.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 struct hash_node_t
 {
     struct node_t* qnode;
@@ -14,3 +18,15 @@ struct hash_table
     int hash_limit;
     struct hash_node_t* table;
 };
+
+int compute_hash(int page, int param);
+
+struct hash_table* hash_init(int npage);
+
+struct hash_create_node* find_page(int page, struct hash_table* table);
+
+void add_page(int page, struct hash_table* table, struct hash_node_t* node);
+
+void delete_page(int page, struct hash_table* table);
+
+void free_hash(struct hash_table* table);
