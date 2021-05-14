d5fab4d6cca142137bfe6d1d919d05897077be84
Pishchev Pavel <pishchev.pm@phystech.edu>
2021-05-14 19:37:18 +0300

    update headers

--- include/cache.h
+++ include/cache.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "qlist.h"
 #include "hash_table.h"
+
+enum {Q_MAIN = 0, Q_IN = 1, Q_OUT = 2, Q_NOTFOUND = -1};
 
 struct cache2q_t
 {
@@ -13,3 +18,15 @@ struct cache2q_t
 
     struct hash_table* hashtable;  
 };
+
+struct cache_result_t
+{
+    char res;
+    int data;
+};
+
+struct cache2q_t* cache2q_init(int main_size, int in_size, int out_size);
+
+void cache2q_free (struct cache2q_t* cache);
+
+struct cache_result_t cache2q_getdata (struct cache2q_t* cache, int page, int (*loaddata)(int));
--- include/hash_table.h
+++ include/hash_table.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "qlist.h"
 
 struct hash_node_t
@@ -9,8 +12,20 @@ struct hash_node_t
     int page;
 };
 
-struct hash_table
+struct hash_table_t
 {
     int hash_limit;
     struct hash_node_t* table;
 };
+
+int compute_hash (int page, int param);
+
+struct hash_table_t* hash_init (int hash_limit);
+
+struct node_t* hash_find_page (struct hash_table_t* htable, int page);
+
+int hash_add_page (struct hash_table_t* htable, int page, struct node_t* qnode);
+
+int hash_delete_page (struct hash_table_t* htable, int page);
+
+void hash_free (struct hash_table_t* htable);
