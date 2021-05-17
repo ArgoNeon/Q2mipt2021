11ebeb2c76d5fffdc2858d6f87896ad31ae577d8
Alex <alxshark@mail.ru>
2021-05-17 09:03:13 +0300

    qlist: добавил коментарии к функциям

--- source/qlist.c
+++ source/qlist.c
@@ -1,13 +1,12 @@
 #include "../include/qlist.h"
 
-
+//initializing a new empty list 
+/// @param [maxsize] maxsize - the maximum number of items in the list
+/// @returns list pointer
 struct list_t* list_init (int maxsize){
 
     void* key_pointer;
     struct list_t* list;
-
-    //maxsize здесь личный параметр списка
-    //список создаётся пустым
 
     assert(maxsize>=0);
 
@@ -26,6 +25,10 @@ struct list_t* list_init (int maxsize){
     return list;
 }
 
+//initializing a new node
+/// @param [page] page - page number
+/// @param [data] data - int content 
+/// @returns node pointer
 struct node_t* pregen_node (int page, int data){
     struct node_t* node = (struct node_t*)calloc(1, sizeof(struct node_t));
 
@@ -41,6 +44,11 @@ struct node_t* pregen_node (int page, int data){
     return node;
 }
 
+/// adding an element to the end 
+/// @param [struct list_t* list] list - list pointer  
+/// @param [struct node_t* node] node - node pointer
+/// @param [struct node_t** disp] disp - pointer to node trash pointer
+/// @returns 0 or 1
 int list_push_back (struct list_t* list, struct node_t* node, struct node_t** disp){
 
     if(node==NULL){
@@ -74,6 +82,11 @@ int list_push_back (struct list_t* list, struct node_t* node, struct node_t** di
     return 0;
 }
 
+/// adding an element to the beggining
+/// @param [struct list_t* list] list - list pointer  
+/// @param [struct node_t* node] node - node pointer
+/// @param [struct node_t** disp] disp - pointer to node trash pointer
+/// @returns 0 or 1
 int list_push_front (struct list_t* list, struct node_t* node, struct node_t** disp){
 
     if(node==NULL){
@@ -107,7 +120,10 @@ int list_push_front (struct list_t* list, struct node_t* node, struct node_t** d
     return 0;
 }
 
-
+/// content check
+/// @param [struct list_t* list] list - list pointer  
+/// @param [struct node_t* node] node - node pointer
+/// @returns 0 or 1
 int list_contains_node (struct list_t* list, struct node_t* node){
     if(node->key_pointer==list->key_pointer){
         return 1;
@@ -115,6 +131,11 @@ int list_contains_node (struct list_t* list, struct node_t* node){
     return 0;
 }
 
+/// remove node from list
+/// @param [struct list_t* list] list - list pointer  
+/// @param [struct node_t* node] node - node pointer
+/// @param [struct node_t** disp] disp - pointer to node trash pointer
+/// @returns 0 or 1
 int list_remove (struct list_t* list, struct node_t* node, struct node_t** disp){
     //проверяем, содержится ли node в list
     if(list_contains_node(list, node)){
@@ -139,6 +160,9 @@ int list_remove (struct list_t* list, struct node_t* node, struct node_t** disp)
     return 1;
 }
 
+/// execute function to list items 
+/// @param [struct list_t* list] list - list pointer  
+/// @param [(*func)(struct node_t*)] func - pointer to func to node
 void list_apply_func (struct list_t* list, void (*func)(struct node_t*)){
     struct node_t* top = list->front;
     while (top != NULL)
@@ -148,6 +172,9 @@ void list_apply_func (struct list_t* list, void (*func)(struct node_t*)){
     }
 }
 
+/// reverse execute function to list items 
+/// @param [struct list_t* list] list - list pointer  
+/// @param [(*func)(struct node_t*)] func - pointer to func to node
 void list_apply_func_reverse (struct list_t* list, void (*func)(struct node_t*)){
     struct node_t* back = list->back;
     while (back != NULL){
@@ -156,15 +183,21 @@ void list_apply_func_reverse (struct list_t* list, void (*func)(struct node_t*))
     }
 }
 
+/// print node function  
+/// @param [struct node_t* node] node - node pointer
 void print_node (struct node_t* node){
     //printf("\n { \n key_pointer: %d \n data: %d \n page: %d \n next: %d; prev: %d ", (int)(node->key_pointer)%1000, (node->data), (node->page),(int)(node->next)%1000,(int)(node->prev)%1000);
     printf("\n { \n data: %d \n page: %d ", (node->data), (node->page));
 }
 
+/// free node function  
+/// @param [struct node_t* node] node - node pointer
 void free_node (struct node_t* node){
     free(node);
 }
 
+/// list free function  
+/// @param [struct list_t* list] list - list pointer  
 void list_free (struct list_t* list){
     struct node_t* top = list->front;
     struct node_t* tmp = NULL;
@@ -176,14 +209,22 @@ void list_free (struct list_t* list){
     free(list);   
 }
 
+/// return list size function 
+/// @param [struct list_t* list] list - list pointer  
 int list_cursize (struct list_t* list){
     return list->size;
 }
 
+/// return list maxsize function 
+/// @param [struct list_t* list] list - list pointer  
 int list_maxsize (struct list_t* list){
     return list->maxsize;
 }
 
+/// function move element to front list
+/// @param [struct list_t* list] list - list pointer  
+/// @param [struct node_t* node] node - node pointer
+/// @returns 0 or 1
 int list_move_front (struct list_t* list, struct node_t* node){
     if(list_contains_node(list, node)){
         struct node_t* tmp = list->front;
@@ -198,6 +239,9 @@ int list_move_front (struct list_t* list, struct node_t* node){
 
 //функция-дополнение к list_is_empty, проверяет соответствие параметра size 
 //и структуры в смысле наличия 
+/// function check exit nature size of list and size
+/// @param [struct list_t* list] list - list pointer  
+/// @returns 0 or 1
 int size_conformity(struct list_t* list){
     if((list->front != NULL || list->back != NULL)  && list->size == 0){
         return 0;
@@ -213,6 +257,9 @@ int size_conformity(struct list_t* list){
 }
 
 //0 если пуст, 1 - не пуст
+/// function check list is empty
+/// @param [struct list_t* list] list - list pointer  
+/// @returns 0 or 1
 int list_is_empty (struct list_t* list){
 
     // ! если список NULL, то он считается некорректным
